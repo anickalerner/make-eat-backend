6ed9a621820a3a652b76409906cc86f19c4db1be
anickalerner <68658833+anickalerner@users.noreply.github.com>
2020-10-18 12:19:19 +0300

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // "dbURL": "mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority",
-  "dbURL": "mongodb+srv://lior:LaS1991426@cluster0.klw8w.mongodb.net/<make_eat_db>?retryWrites=true&w=majority",
-}
-
-
-// module.exports = {
-//   "dbURL": "mongodb://localhost:27017",
-// }
